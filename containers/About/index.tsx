0f55b7cfa1284183137f6cb74e5068acf565e0f6
glenn-osc <GLe@cityofsydney.nsw.gov.au>
2023-08-31 14:56:22 +0700

    update banner

--- containers/About/index.tsx
+++ containers/About/index.tsx
@@ -9,7 +9,7 @@ const About: React.FC = ({ }) => {
 
     return (
         <div className="flex justify-center items-center w-screen h-screen bg-slate-950">
-            <div className="w-10/12 flex items-center">
+            <div className="w-full flex flex-col sm:w-10/12 sm:flex-row items-center">
                 <div className="flex w-1/2 justify-end mr-9">
                     <img
                         src={'/avatar.png'}
@@ -20,7 +20,7 @@ const About: React.FC = ({ }) => {
                     initial={{ x: -100, opacity: 0 }} // Initial position and opacity
                     animate={{ x: 0, opacity: 1 }}    // Final position and opacity
                     transition={{ duration: 0.5 }}   // Animation duration
-                    className="flex flex-col items-start w-1/2 ml-9"
+                    className="w-full mt-[320px] flex flex-col items-start sm:w-1/2 sm:mt-0 ml-9"
                 >
                     <div className="font-['Poppins-ExtraBold'] text-6xl mb-2">Hey! I'm a</div>
                     <div className="font-['Poppins-ExtraBold'] text-5xl text-orange-400 mb-2 min-h-[48px]"><Typewriter texts={titles}/></div>
--- containers/Banner/index.tsx
+++ containers/Banner/index.tsx
@@ -9,18 +9,18 @@ const Banner: React.FC = ({ }) => {
 
     return (
         <div className="flex justify-center items-center w-screen h-screen bg-transparent pt-[75px] px-[50px] sticky top-0 left-0 z-[-1]">
-            <div className="w-10/12 flex items-center">
+            <div className="w-full flex flex-col sm:w-10/12 sm:flex-row items-center">
                 <motion.div
                     initial={{ x: -100, opacity: 0 }} // Initial position and opacity
                     animate={{ x: 0, opacity: 1 }}    // Final position and opacity
                     transition={{ duration: 0.5 }}   // Animation duration
-                    className="flex flex-col items-start w-1/2"
+                    className="w-full mt-[320px] flex flex-col items-start sm:w-1/2 sm:mt-0"
                 >
                     <div className="font-['Poppins-ExtraBold'] text-6xl mb-2">Hey! I'm a</div>
                     <div className="font-['Poppins-ExtraBold'] text-5xl text-orange-400 mb-2 min-h-[48px]"><Typewriter texts={titles}/></div>
                     <div className="text-slate-300 text-lg">Obviously I'm a Web Designer. Web Developer with over 7 years of experience. Experienced with all stages of the development.</div>
                 </motion.div>
-                <div className="flex w-1/2">
+                <div className="w-full justify-center flex sm:w-1/2">
                     <img src={'/picture-banner.png'} className="w-[700px] h-[700px] min-w-[500px] min-h-[500px] object-contain"/>
                 </div>
             </div>
